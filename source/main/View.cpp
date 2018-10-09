ec5de932e3f0ca099cd011642256e4e07a15404b
PhotoScan <photoscan@ln-mowifi-02-10.1.154.253.brighton.ac.uk>
2018-10-09 13:02:39 +0100

    new folder structure and a class used for the View aspect of the game

---
+++ source/main/View.cpp
@@ -0,0 +1,90 @@
+#include "View.hpp"
+
+View::View(int widtht, int heightt) {
+    width = widtht;
+    height = heightt;
+   
+}
+View::~View() {
+    
+}
+
+int View::initialise() {
+    
+    // Initialise GLFW
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        getchar();
+        return -1;
+    }
+    
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+
+    
+    // Open a window and create its OpenGL context
+    window = glfwCreateWindow(width, height, "My Game", NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    
+    // Set the mouse at the center of the screen
+    
+    glfwPollEvents();
+    //we set the cursor at the centre so that it always start on our origin (0,0,0)
+    glfwSetCursorPos(window, width / 2, height / 2);
+    
+    
+    // Dark blue background
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    
+    // Initialize GLEW
+    glewExperimental = true; // Needed for core profile
+    
+    
+    return 1;
+}
+
+void View::update() {
+    
+    do{
+        // Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+        glClear( GL_COLOR_BUFFER_BIT );
+        
+        // Draw nothing, see you in tutorial 2 !
+        
+        
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        
+    } // Check if the ESC key was pressed or the window was closed
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+          glfwWindowShouldClose(window) == 0 );
+    
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
+    
+}
+
---
+++ source/main/View.hpp
@@ -0,0 +1,46 @@
+#pragma once
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <glfw3.h>
+#include <vector>
+
+// Include GLM
+#include <glm/glm.hpp>
+using namespace glm;
+#include "common/objloader.hpp"
+#include "common/vboindexer.hpp"
+#include <glm/gtx/transform.hpp>
+#include "common/shader.hpp"
+#include "common/controls.hpp"
+#include "common/text2D.hpp"
+
+class View {
+private:
+    
+    GLFWwindow* window;
+    
+    GLuint programID;
+  
+    
+    GLuint VertexArrayID;
+    
+    
+    //screen size
+    int width;
+    int height;
+  
+    
+    
+    
+    
+public:
+    View(int widtht, int heightt);
+    ~View();
+    int initialise();
+    void update();
+
+    
+};
+
